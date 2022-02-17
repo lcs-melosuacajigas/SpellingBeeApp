c9ab22c9ab56ca49fb72344582e177da09e17c57
Mateo Elosua <melosua@lcs.on.ca>
2022-02-17 11:30:20 -0500

    Initial Commit

---
+++ SpellingBeeApp/SpellingBeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBeeAppApp.swift
+//  SpellingBeeApp
+//
+//  Created by Mateo Elosua on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
